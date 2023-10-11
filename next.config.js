e7b8720f0bfed4420f5a394647306cc34beccac4
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2023-10-11 15:54:50 +0300

    ts ignore build error

--- next.config.js
+++ next.config.js
@@ -28,6 +28,9 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
