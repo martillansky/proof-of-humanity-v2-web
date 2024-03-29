dc2b959ca086ed6fcaae91e2564363487a3427ad
Martin <martin.moguillansky@gmail.com>
2024-03-29 19:48:27 -0300

    IPFS gateway env vars. Chiado setup (waiting for thegraph to support chiado by April 12).

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -1,6 +1,6 @@
 import { getSdk } from "generated/graphql";
 import { GraphQLClient } from "graphql-request";
-import { gnosis, sepolia } from "viem/chains";
+import { gnosis, sepolia, /* gnosisChiado */ } from "viem/chains";
 import { SupportedChainId } from "./chains";
 
 export type sdkReturnType = ReturnType<typeof getSdk>;
@@ -21,4 +21,10 @@ export const sdk = {
       "https://api.studio.thegraph.com/query/64099/proof-of-humanity-gnosis/version/latest"
     )
   ),
+  /* [gnosisChiado.id]: getSdk(
+    new GraphQLClient(
+      "https://api.studio.thegraph.com/query/64099/proof-of-humanity-gnosis/version/latest"
+      //"https://api.studio.thegraph.com/query/64099/proof-of-humanity-chiado/version/latest"
+    )
+  ), */
 };
--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
-import { IPFS_GATEWAY } from "../config";
 
 export const uploadToIPFS = async (data: FormData) => {
   const result = await axios.post("/api/ipfs-upload", data);
   return result.data.uri;
 };
 
-export const ipfs = (uri: string) => `https://${IPFS_GATEWAY}${uri}`;
-//export const ipfs = (uri: string) => `https://ipfs.kleros.io${uri}`;
+export const ipfs = (uri: string) => `https://${process.env.REACT_APP_IPFS_GATEWAY}${uri}`;
 
 export const ipfsFetch = async <F>(ipfsURI: string) =>
   (await axios.get(ipfs(ipfsURI))).data as F;
