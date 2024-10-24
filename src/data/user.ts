42edc65a9e9345d7aaf305e342853b894e242bd2
Martin <martin.moguillansky@gmail.com>
2024-10-24 12:21:52 -0300

    fix: registration issue at legacy subgraph
    This fix must be temporary. Once the legacy subgraph is fixed and redeployed, sanitize function in user.ts must be removed

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,10 +1,23 @@
 import { supportedChains } from "config/chains";
 import { sdk } from "config/subgraph";
+import { MeQuery } from "generated/graphql";
+
+// This fixes an error in the legacy subgraph were registration has not
+// been removed as expected. Once solved the issue at subgraph level this
+// function should be removed
+const sanitize = (res: MeQuery[]) => {
+  res.map((claimer) => {
+    if (claimer.claimer?.currentRequest && claimer.claimer?.registration)
+      claimer.claimer.registration = null;
+  });
+};
 
 export const getMyData = async (account: string) => {
   const res = await Promise.all(
     supportedChains.map((chain) => sdk[chain.id].Me({ id: account })),
   );
+
+  sanitize(res);
 
   const homeChain = supportedChains.find(
     (_, i) => res[i].claimer?.registration,
